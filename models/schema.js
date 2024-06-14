8445ac5142967156ddb7b69110c343c79ddbba22
Rilave <rilavej1991@aluno.ifsc.edu.br>
2024-06-14 15:46:44 -0300

    craição de tabela junto com schema

--- models/schema.js
+++ models/schema.js
@@ -11,4 +11,31 @@ try {
     console.log(err)
 }
 
-export default pool
+var lista = ["tarefa", "titulo", "descricao", "prazo", "status"]
+
+// async function createTable(lista) {
+
+    try {
+        console.log("### CREATE TABLE IF NOT EXISTS")
+
+        const result = await pool.execute(
+            `CREATE TABLE IF NOT EXISTS ${lista[0]} (
+                id INT AUTO_INCREMENT PRIMARY KEY, 
+                ${lista[1]} VARCHAR(50), 
+                ${lista[2]} VARCHAR(50), 
+                ${lista[3]} VARCHAR(50), 
+                ${lista[4]} VARCHAR(50)
+            )`,
+            lista
+        )
+        console.log(result)
+
+    } catch (err) {
+        console.log(err)
+    }
+// }
+// createTable(lista)
+
+
+
+// export default pool
--- models/tarefas.js
+++ models/tarefas.js
@@ -1,29 +1,6 @@
-import pool from "./schema.js"
+import pool from "../config/pool.js"
 
- var lista = [`tarefa`, `titulo`, `descricao`, `prazo`, `status`]
 
-// async function createTable(lista) {
-
-    try {
-        console.log("### CREATE TABLE IF NOT EXISTS")
-
-        const result = await pool.query(
-            `CREATE TABLE IF NOT EXISTS ${lista[0]} (
-                id INT AUTO_INCREMENT PRIMARY KEY, 
-                ${lista[1]} VARCHAR(50), 
-                ${lista[2]} VARCHAR(50), 
-                ${lista[3]} VARCHAR(50), 
-                ${lista[4]} VARCHAR(50)
-            )`/* ,
-            lista */
-            )
-        console.log(result)
-
-    } catch (err) {
-        console.log(err)
-    }
-// }
-// createTable(`tarefa`, `titulo`, `descricao`, `prazo`, `status`)
 
 
 async function getTarefas() {
