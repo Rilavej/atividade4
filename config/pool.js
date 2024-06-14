2c0c05c2cfda3d65f2cb4e17f07f1db536bd4acd
Rilave <rilavej1991@aluno.ifsc.edu.br>
2024-06-14 15:04:09 -0300

    adicionado config/pool.js

---
+++ config/pool.js
@@ -0,0 +1,27 @@
+import { createPool } from "mysql2/promise";
+
+import { config } from "dotenv";
+config()
+
+const pool = await (async () => {
+    
+    try {
+        const pool = createPool({
+            host: process.env.MYSQL_HOST, 
+            user: process.env.MYSQL_USER, 
+            password: process.env.MYSQL_PASSWORD, 
+        })
+        
+        console.log("Conexão com servidor de banco de dados estabelecida.")
+        return pool
+
+        // const connection = await pool.getConnection()
+
+    }
+    catch (err) {
+        console.log(err)
+    }
+
+})();
+
+export default pool
